67fa721e0ee16712c88b808c880fa972b4074231
Mark Gavrilyuk <mark.gvrlk@mail.ru>
2024-06-23 09:49:48 +0300

    Создание коммита и выполнение урока

---
+++ script.js
@@ -0,0 +1,10 @@
+/* confirm = Да или Нет | true or false
+let answer = confirm("Ты крутой парень?")
+
+promt = всегда приходит в виде строки 
+let answer2 = promt("Ты крутой парень? Если да, то напиши это") */
+
+'use strict';
+
+
+console.log(5 + 5);
